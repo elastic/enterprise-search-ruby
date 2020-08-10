8bcf0b20983e03a9a60257091eb157d836b39c89
Fernando Briano <fernando@picandocodigo.net>
2020-08-10 22:37:00 +0100

    Generated Workplace Search API code

---
+++ lib/elastic/workplace-search/api/delete_documents.rb
@@ -0,0 +1,43 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+module Elastic
+  module EnterpriseSearch
+    module WorkplaceSearch
+      module Actions
+        # Documents - Deletes a list of documents from a custom content source
+        # Remove documents from a Custom API Source
+        #
+        # @option content_source_key - Unique key for a Custom API source, provided upon creation of a Custom API Source (*Required*)
+        #
+        # @see https://www.elastic.co/guide/en/workplace-search/current/workplace-search-custom-sources-api.html#destroy
+        #
+        def delete_documents(content_source_key, parameters = {})
+          raise ArgumentError, "Required parameter 'content_source_key' missing" unless content_source_key
+
+          request(
+            :post,
+            "/api/ws/v1/sources/#{content_source_key}/documents/bulk_destroy",
+            parameters
+          )
+        end
+
+        alias_method :destroy_documents, :delete_documents
+      end
+    end
+  end
+end
