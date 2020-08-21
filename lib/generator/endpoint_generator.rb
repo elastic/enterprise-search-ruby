5faf655bce3e39d331fdf7eba8bd9071945c37a9
Fernando Briano <fernando@picandocodigo.net>
2020-08-21 23:16:58 +0100

    Extracts parameters processing into module

--- lib/generator/endpoint_generator.rb
+++ lib/generator/endpoint_generator.rb
@@ -21,12 +21,14 @@ require 'json'
 require 'erb'
 require_relative './utils.rb'
 require_relative './documentation_helper.rb'
+require_relative './parameters_helper.rb'
 
 module Elastic
   module Generator
     # Generates code for REST API Endpoints
     class EndpointGenerator
       include DocumentationHelper
+      include ParametersHelper
       CURRENT_PATH = File.dirname(__FILE__).freeze
 
       def initialize(name)
@@ -89,102 +91,6 @@ module Elastic
         @method_name = Utils.to_ruby_name(endpoint['operationId'])
         setup_parameters!(endpoint)
         @doc = setup_documentation(endpoint)
-      end
-
-      def setup_parameters!(endpoint)
-        # TODO - Follow $ref to document parameters from body
-        endpoint['parameters'].each { |param| add_parameter(param) } if endpoint.dig('parameters')
-        extract_path_parameters.each do |param|
-          next unless @params.select { |p| p['name'] == param }.empty?
-
-          @params << {
-            'name' => param,
-            'required' => true,
-            'description' => '',
-            'type' => 'String'
-          }
-        end
-      end
-
-      def extract_path_parameters
-        @path.scan(/{(\w+)}/).flatten
-      end
-
-      def add_parameter(param)
-        @params << if param.keys.include?('$ref') && !param['$ref'].empty?
-                     parameter_name_and_description(Utils.dig_ref_from_spec(param['$ref'], @spec))
-                   else
-                     parameter_name_and_description(param)
-                   end
-      end
-
-      def parameter_name_and_description(param)
-        param['name'] = Utils.to_ruby_name(param['x-codegen-param-name']) if param['x-codegen-param-name']
-
-        # Check the spec for parameters with a given name and retrieve info
-        param_info = @spec.dig('components', 'parameters').select do |_, p|
-          p['name'] == param['name']
-        end.values.first
-        param_info = param if param_info.nil?
-
-        parameter_display(param['name'], param_info)
-      end
-
-      def parameter_display(name, param_info)
-        {
-          'name' => name,
-          'description' => param_info['description'],
-          'type' => param_info.dig('schema', 'type') || param_info.dig('type'),
-          'required' => param_info['required']
-        }
-      end
-
-      def required_params
-        return [] unless @params
-
-        @params.select { |p| p['required'] }
-      end
-
-      METHOD_SIGNATURE_PARAMS = [
-        'content_source_key'
-      ].freeze
-
-      def method_signature_params
-        return unless @params
-
-        required_params.select do |param|
-          in_signature?(param['name'])
-        end.map { |p| p['name'] }
-      end
-
-      def in_signature?(param_name)
-        METHOD_SIGNATURE_PARAMS.include?(param_name)
-      end
-
-      def raise_for_required_params
-        errors = []
-        required_params.map do |param|
-          name = param['name']
-          parameter = in_signature?(name) ? name : "parameters[:#{name}]"
-          errors << "raise ArgumentError, \"Required parameter '#{name}' missing\" unless #{parameter}"
-        end
-        errors.join("\n")
-      end
-
-      def display_signature_params
-        # Display
-        params = method_signature_params
-        params << 'body = {}' if body?
-        params << 'parameters = {}'
-        "(#{params.join(', ')})"
-      end
-
-      def request_method_params
-        params = []
-        params += [":#{@http_method}", "\"#{@path}\""]
-        params << 'parameters' if @params
-        params << 'body' if body?
-        params.join(",\n")
       end
 
       def body?
