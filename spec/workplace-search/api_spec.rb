5fb31a0eb0ca49e87397fc0b206877b0dd1ceec2
Fernando Briano <fernando@picandocodigo.net>
2020-09-17 19:20:16 +0100

    [WorkplaceSearch] Adds Permissions API specs

--- spec/workplace-search/api_spec.rb
+++ spec/workplace-search/api_spec.rb
@@ -81,4 +81,113 @@ describe Elastic::EnterpriseSearch::WorkplaceSearch::Client do
       end
     end
   end
+
+  context 'permissions' do
+    let(:user) { 'enterprise_search' }
+
+    it 'lists permissions' do
+      VCR.use_cassette(:list_permissions) do
+        response = client.list_permissions(content_source_key)
+
+        expect(response.status).to eq 200
+
+        expect(response.body)
+          .to eq(
+            { 'meta' =>
+              { 'page' => { 'current' => 1, 'total_pages' => 1, 'total_results' => 1, 'size' => 25 } }, 'results' => [
+                { 'user' => 'enterprise_search', 'permissions' => [] }
+              ] }
+          )
+      end
+    end
+
+    it 'gets user permissions' do
+      clear_user_permissions
+
+      VCR.use_cassette(:user_permissions_empty) do
+        response = client.user_permissions(content_source_key, { user: user })
+
+        expect(response.status).to eq 200
+
+        expect(response.body)
+          .to eq(
+            { 'user' => 'enterprise_search', 'permissions' => [] }
+          )
+      end
+    end
+
+    def clear_user_permissions
+      VCR.use_cassette(:clear_user_permissions) do
+        client.put_user_permissions(
+          content_source_key,
+          { permissions: [] },
+          { user: user }
+        )
+      end
+    end
+
+    it 'updates user permissions' do
+      VCR.use_cassette(:put_user_permissions) do
+        response = client.add_user_permissions(
+          content_source_key,
+          { permissions: ['testing', 'more', 'permissions'] },
+          { user: user }
+        )
+
+        expect(response.status).to eq 200
+
+        expect(response.body)
+          .to eq(
+            { 'user' => 'enterprise_search', 'permissions' => ['testing', 'more', 'permissions'] }
+          )
+
+        response = client.put_user_permissions(
+          content_source_key,
+          { permissions: [] },
+          { user: user }
+        )
+
+        expect(response.status).to eq 200
+
+        expect(response.body)
+          .to eq(
+            { 'user' => 'enterprise_search', 'permissions' => [] }
+          )
+      end
+    end
+
+    it 'adds and removes permissions from a user' do
+      VCR.use_cassette(:add_user_permissions) do
+        permissions = ['permission1', 'permission2']
+        response = client.add_user_permissions(
+          content_source_key,
+          { permissions: permissions },
+          { user: user }
+        )
+
+        expect(response.status).to eq 200
+
+        expect(response.body)
+          .to eq(
+            { 'user' => 'enterprise_search', 'permissions' => ['permission1', 'permission2'] }
+          )
+      end
+
+      VCR.use_cassette(:remove_user_permissions) do
+        permissions = ['permission1', 'permission2']
+        response = client.remove_user_permissions(
+          content_source_key,
+          { permissions: permissions },
+          { user: user }
+        )
+
+        expect(response.status).to eq 200
+
+        expect(response.body)
+          .to eq(
+            { 'user' => 'enterprise_search', 'permissions' => [] }
+          )
+      end
+    end
+  end
 end
