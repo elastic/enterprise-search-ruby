6ec5ecfe246df3e429ff18e671bd73bfdeb207fd
Fernando <fernando@picandocodigo.net>
2021-02-05 17:19:04 +0000

    [DOCS] Better source documentation for body properties, oauth

--- lib/elastic/app-search/api/create_curation.rb
+++ lib/elastic/app-search/api/create_curation.rb
@@ -26,8 +26,8 @@ module Elastic
         # @param engine_name [String]  (*Required*)
         # @param arguments [Hash] endpoint arguments
         # @option arguments [Array] :queries List of affected search queries (*Required*)
-        # @option arguments [] :promoted_doc_ids List of promoted document IDs
-        # @option arguments [] :hidden_doc_ids List of hidden document IDs
+        # @option arguments :promoted_doc_ids List of promoted document IDs
+        # @option arguments :hidden_doc_ids List of hidden document IDs
         # @option arguments [Hash] :body The request body
         # @option arguments [Hash] :headers optional HTTP headers to send with the request
         #
