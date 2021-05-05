15fa89cfd2d02abf2059a8cf025b8c0e4df74ba5
Fernando Briano <fernando@picandocodigo.net>
2021-05-05 16:43:02 +0100

    [Workplace Search] Migrate external identities specs to integration

--- spec/integration/workplace_search_spec.rb
+++ spec/integration/workplace_search_spec.rb
@@ -125,4 +125,75 @@ describe Elastic::EnterpriseSearch::WorkplaceSearch::Client do
       expect(response.status).to eq 200
     end
   end
+
+  context 'External Identities' do
+    let(:content_source_id) { client.create_content_source(name: 'my_content').body['id'] }
+    let(:user) { 'elastic_user' }
+    let(:source_user_id) { 'example@elastic.co' }
+    let(:body) do
+      { user: user, source_user_id: source_user_id }
+    end
+
+    context 'Creates' do
+      let(:source_user_id) { 'test@elastic.co' }
+      let(:user) { 'test' }
+
+      it 'creates an external identity' do
+        response = client.create_external_identity(content_source_id, body: body)
+
+        expect(response.status).to eq 200
+        expect(response.body).to eq({ 'source_user_id' => source_user_id, 'user' => user })
+
+        client.delete_external_identity(content_source_id, user: user)
+      end
+
+      it 'creates and retrieves' do
+        client.create_external_identity(content_source_id, body: body)
+        response = client.external_identity(content_source_id, user: user)
+
+        expect(response.status).to eq 200
+        expect(response.body).to eq({ 'source_user_id' => source_user_id, 'user' => user })
+        client.delete_external_identity(content_source_id, user: user)
+      end
+    end
+
+    context 'Lists' do
+      let(:user) { 'lists_test' }
+
+      it 'lists external identities' do
+        client.create_external_identity(content_source_id, body: body)
+        response = client.list_external_identities(content_source_id)
+
+        expect(response.status).to eq 200
+        expect(response.body['results']).to eq([{ 'source_user_id' => source_user_id, 'user' => user }])
+        client.delete_external_identity(content_source_id, user: user)
+      end
+    end
+
+    context 'Updates' do
+      before do
+        client.create_external_identity(content_source_id, body: body)
+      end
+
+      it 'updates an external identity' do
+        body = { source_user_id: 'example2@elastic.co' }
+        response = client.put_external_identity(content_source_id, user: user, body: body)
+
+        expect(response.status).to eq 200
+        expect(response.body).to eq({ 'source_user_id' => 'example2@elastic.co', 'user' => user })
+      end
+    end
+
+    context 'Deletes' do
+      before do
+        client.create_external_identity(content_source_id, body: body)
+      end
+
+      it 'deletes an external identity' do
+        response = client.delete_external_identity(content_source_id, user: user)
+        expect(response.status).to eq 200
+        expect(response.body).to eq 'ok'
+      end
+    end
+  end
 end
