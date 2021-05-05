1fda49b730909cb8e61c1df74af529a2f5dec100
Fernando Briano <fernando@picandocodigo.net>
2021-05-05 14:07:01 +0100

    [Workplace Search] Adds document endpoints
    - document: retrieves a document in a content source
    - delete_all_documents: deletes all documents in a content source

---
+++ lib/elastic/workplace-search/api/document.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+module Elastic
+  module EnterpriseSearch
+    module WorkplaceSearch
+      module Actions
+        # Documents - Retrieves a document by ID from the specified content source
+        # Retrieves a document by ID from the specified content source
+        #
+        # @param content_source_id [String] Unique ID for a Custom API source, provided upon creation of a Custom API Source (*Required*)
+        # @param arguments [Hash] endpoint arguments
+        # @option arguments [String] :document_id  (*Required*)
+        # @option arguments [Hash] :headers optional HTTP headers to send with the request
+        #
+        # @see https://www.elastic.co/guide/en/workplace-search/current/workplace-search-content-sources-api.html#get-document-by-id-api
+        #
+        def document(content_source_id, arguments = {})
+          raise ArgumentError, "Required parameter 'content_source_id' missing" unless content_source_id
+          raise ArgumentError, "Required parameter 'document_id' missing" unless arguments[:document_id]
+
+          document_id = arguments[:document_id]
+
+          headers = arguments.delete(:headers) || {}
+
+          request(
+            :get,
+            "api/ws/v1/sources/#{content_source_id}/documents/#{document_id}/",
+            arguments,
+            {},
+            headers
+          )
+        end
+      end
+    end
+  end
+end
