6b91a97ed5598f69ff00d45605c522536e7f859c
Fernando Briano <fernando@picandocodigo.net>
2021-08-09 19:07:51 +0100

    [Workplace Search] Adds create_batch_synonym_sets

---
+++ lib/elastic/workplace-search/api/create_batch_synonym_sets.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+module Elastic
+  module EnterpriseSearch
+    module WorkplaceSearch
+      module Actions
+        # Synonyms - Create a batch of synonym sets
+        # Create batched synonym sets
+        #
+        # @param arguments [Hash] endpoint arguments
+        # @option arguments [Hash] :body
+        # @option body :synonyms
+        # @option body :synonym_sets
+        # @option arguments [Hash] :headers optional HTTP headers to send with the request
+        #
+        # @see https://www.elastic.co/guide/en/workplace-search/current/workplace-synonyms-api.html#create-synonyms
+        #
+        def create_batch_synonym_sets(arguments = {})
+          body = arguments.delete(:body) || {}
+          headers = arguments.delete(:headers) || {}
+
+          request(
+            :post,
+            'api/ws/v1/synonyms/',
+            arguments,
+            body,
+            headers
+          )
+        end
+      end
+    end
+  end
+end
