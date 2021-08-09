0d8d86bdc5d6c42dfd2baa131df16e934419848f
Fernando Briano <fernando@picandocodigo.net>
2021-08-09 18:11:38 +0100

    [Workplace Search] Updates APIs source code documentation

--- lib/elastic/workplace-search/api/create_content_source.rb
+++ lib/elastic/workplace-search/api/create_content_source.rb
@@ -25,11 +25,13 @@ module Elastic
         # Create a custom content source
         #
         # @param arguments [Hash] endpoint arguments
-        # @option arguments [Object] :body Definition to create a Workplace Search Content Source (Required: name)
-        # @option body [String] :name The human readable display name for this Content Source.
+        # @option arguments [Hash] :body Definition to create a Workplace Search Content Source (Required: name)
+        # @option body [String] :name The human readable display name for this Content Source. (*Required)
         # @option body :schema The schema that each document in this Content Source will adhere to.
         # @option body :display The display details which governs which fields will be displayed, and in what order, in the search results.
         # @option body [Boolean] :is_searchable Whether or not this Content Source will be searchable on the search page.
+        # @option body :indexing
+        # @option body :facets
         # @option arguments [Hash] :headers optional HTTP headers to send with the request
         #
         # @see https://www.elastic.co/guide/en/workplace-search/current/workplace-search-content-sources-api.html#create-content-source-api
