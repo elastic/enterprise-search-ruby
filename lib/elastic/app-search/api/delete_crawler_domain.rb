a00a99a5eb28f86a8a30f41be516ec196f8b4c8f
Fernando Briano <fernando@picandocodigo.net>
2021-09-20 21:49:06 +0100

    [App Search] Adds [create,delete] crawler domain and validation result

---
+++ lib/elastic/app-search/api/delete_crawler_domain.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+module Elastic
+  module EnterpriseSearch
+    module AppSearch
+      module Actions
+        # Crawler - Delete domain configuration
+        # Deletes crawler domain configuration for a given domain
+        #
+        # @param engine_name [String] Name of the engine (*Required*)
+        # @param arguments [Hash] endpoint arguments
+        # @option arguments [String] :domain_id Crawler Domain ID (*Required*)
+        # @option arguments [Hash] :body The request body
+        # @option arguments [Hash] :headers optional HTTP headers to send with the request
+        #
+        # @see https://www.elastic.co/guide/en/app-search/current/web-crawler-api-reference.html#web-crawler-apis-delete-crawler-domain
+        #
+        def delete_crawler_domain(engine_name, arguments = {})
+          raise ArgumentError, "Required parameter 'engine_name' missing" unless engine_name
+          raise ArgumentError, "Required parameter 'domain_id' missing" unless arguments[:domain_id]
+
+          domain_id = arguments[:domain_id]
+          body = arguments.delete(:body) || {}
+          headers = arguments.delete(:headers) || {}
+
+          request(
+            :delete,
+            "api/as/v0/engines/#{engine_name}/crawler/domains/#{domain_id}/",
+            arguments,
+            body,
+            headers
+          )
+        end
+      end
+    end
+  end
+end
---
+++ spec/integration/app-search/crawler_domain_spec.rb
@@ -0,0 +1,58 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+# frozen_string_literal: true
+
+require_relative "#{__dir__}/app_search_helper.rb"
+
+describe Elastic::EnterpriseSearch::AppSearch::Client do
+  context 'Web Crawler' do
+    let(:engine_name) { 'crawler' }
+    let(:name) { 'https://www.elastic.co' }
+
+    before do
+      client.create_engine(name: engine_name)
+    end
+
+    after do
+      client.delete_crawler_domain(engine_name, domain_id: @domain['id'])
+      client.delete_engine(engine_name)
+      sleep 1
+    end
+
+    it 'creates a domain' do
+      body = { name: name }
+      response = client.create_crawler_domain(engine_name, body: body)
+      @domain = response.body
+
+      expect(response.status).to eq 200
+      expect(response.body.keys).to include('id')
+      expect(response.body).to include('name' => name)
+    end
+
+    it 'validates a domain' do
+      body = { name: name }
+      @domain = client.create_crawler_domain(engine_name, body: body).body
+      body = { url: name }
+      response = client.crawler_domain_validation_result(body: body)
+
+      expect(response.status).to eq 200
+      expect(response.body['url']).to eq name
+      expect(response.body['valid']).to eq true
+    end
+  end
+end
