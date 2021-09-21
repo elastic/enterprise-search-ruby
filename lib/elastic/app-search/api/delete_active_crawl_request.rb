b5d41ab5ef1a1282f7591757bf914618fdd8ca87
Fernando Briano <fernando@picandocodigo.net>
2021-09-21 10:42:15 +0100

    [App Search] Adds crawler requests

---
+++ lib/elastic/app-search/api/delete_active_crawl_request.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+module Elastic
+  module EnterpriseSearch
+    module AppSearch
+      module Actions
+        # Crawler - Cancel an active crawl request
+        # Cancels an active crawl request, stopping a running crawl if needed.
+        #
+        # @param engine_name [String] Name of the engine (*Required*)
+        # @param arguments [Hash] endpoint arguments
+        # @option arguments [Hash] :body The request body
+        # @option arguments [Hash] :headers optional HTTP headers to send with the request
+        #
+        # @see https://www.elastic.co/guide/en/app-search/current/web-crawler-api-reference.html#web-crawler-apis-post-crawler-crawl-requests-active-cancel
+        #
+        def delete_active_crawl_request(engine_name, arguments = {})
+          raise ArgumentError, "Required parameter 'engine_name' missing" unless engine_name
+
+          body = arguments.delete(:body) || {}
+          headers = arguments.delete(:headers) || {}
+
+          request(
+            :post,
+            "api/as/v0/engines/#{engine_name}/crawler/crawl_requests/active/cancel/",
+            arguments,
+            body,
+            headers
+          )
+        end
+      end
+    end
+  end
+end
