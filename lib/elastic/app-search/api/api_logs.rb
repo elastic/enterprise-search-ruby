a5f1142012dfa5089085d9ed0cb59c62b73023e7
Fernando Briano <fernando@picandocodigo.net>
2021-09-23 10:25:46 +0100

    [App Search] Updates API logs

--- lib/elastic/app-search/api/api_logs.rb
+++ lib/elastic/app-search/api/api_logs.rb
@@ -21,27 +21,28 @@ module Elastic
   module EnterpriseSearch
     module AppSearch
       module Actions
-        # Logs - The API Log displays API request and response data at the Engine level
+        # APILogs - Retrieve API logs
+        # The API Log displays API request and response data at the Engine level
         #
-        # @param engine_name [String]  (*Required*)
+        # @param engine_name [String] Name of the engine (*Required*)
         # @param arguments [Hash] endpoint arguments
-        # @option arguments [String] :from_date Filter date from (*Required*)
-        # @option arguments [String] :to_date Filter date to (*Required*)
-        # @option arguments [Integer] :current_page The page to fetch. Defaults to 1
-        # @option arguments [Integer] :page_size The number of results per page
-        # @option arguments [String] :query Use this to specify a particular endpoint, like analytics, search, curations and so on
-        # @option arguments [String] :http_status_filter Filter based on a particular status code: 400, 401, 403, 429, 200
-        # @option arguments [String] :http_method_filter Filter based on a particular HTTP method: GET, POST, PUT, PATCH, DELETE
-        # @option arguments [String] :sort_direction Would you like to have your results ascending, oldest to newest, or descending, newest to oldest?
-        # @option arguments [Hash] :body The request body
+        # @option arguments [Date] :from_date Filter date from (*Required*)
+        # @option arguments [Date] :to_date Filter date to (*Required*)
+        # @option arguments [Hash] :body
+        # @option body [String] :query You can search over the full_request_path of an API Log event. Use this to specify a particular endpoint, like analytics, search, curations and so on.
+        # @option body [String] :sort_direction Would you like to have your results ascending, oldest to newest, or descending, newest to oldest? Accepts asc or desc. Defaults to ascending.
+        # @option body [Integer] :page current for current page, total_pages for the net number of pages, total_results for the overall number of results, size for the amount of results per page.
+        # @option body :filters
+        # @option filters [String] :status Filter based on a particular status code: 400, 401, 403, 429, 200
+        # @option filters [String] :method Filter based on a particular HTTP method: GET, POST, PUT, PATCH, DELETE
         # @option arguments [Hash] :headers optional HTTP headers to send with the request
         #
         # @see https://www.elastic.co/guide/en/app-search/current/api-logs.html
         #
         def api_logs(engine_name, arguments = {})
+          raise ArgumentError, "Required parameter 'engine_name' missing" unless engine_name
           raise ArgumentError, "Required parameter 'from_date' missing" unless arguments[:from_date]
           raise ArgumentError, "Required parameter 'to_date' missing" unless arguments[:to_date]
-          raise ArgumentError, "Required parameter 'engine_name' missing" unless engine_name
 
           body = arguments.delete(:body) || {}
           headers = arguments.delete(:headers) || {}
@@ -49,7 +50,7 @@ module Elastic
           arguments['filters[date][to]'] = date_to_rfc3339(arguments.delete(:to_date))
 
           request(
-            :get,
+            :post,
             "api/as/v1/engines/#{engine_name}/logs/api/",
             arguments,
             body,
