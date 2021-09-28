9583463291a4a27e024204ef624985c326463798
Fernando Briano <fernando@picandocodigo.net>
2021-09-28 10:46:48 +0100

    [App Search] Top Clicks Analytics uses body, updated docs

--- lib/elastic/app-search/api/top_clicks_analytics.rb
+++ lib/elastic/app-search/api/top_clicks_analytics.rb
@@ -21,15 +21,15 @@ module Elastic
   module EnterpriseSearch
     module AppSearch
       module Actions
-        # Analytics - Returns the number of clicks received by a document in descending order
+        # Analytics - Query for analytics click data
+        # Returns the number of clicks received by a document in descending order
         #
-        # @param engine_name [String]  (*Required*)
+        # @param engine_name [String] Name of the engine (*Required*)
         # @param arguments [Hash] endpoint arguments
-        # @option arguments [String] :query Filter clicks over a search query
-        # @option arguments [Integer] :current_page The page to fetch. Defaults to 1
-        # @option arguments [Integer] :page_size The number of results per page
-        # @option arguments [Array] :filters Analytics filters
-        # @option arguments [Hash] :body The request body
+        # @option arguments [Hash] :body
+        # @option body [String] :query
+        # @option body :filters
+        # @option body [Object] :page
         # @option arguments [Hash] :headers optional HTTP headers to send with the request
         #
         # @see https://www.elastic.co/guide/en/app-search/current/clicks.html
@@ -41,7 +41,7 @@ module Elastic
           headers = arguments.delete(:headers) || {}
 
           request(
-            :get,
+            :post,
             "api/as/v1/engines/#{engine_name}/analytics/clicks/",
             arguments,
             body,
