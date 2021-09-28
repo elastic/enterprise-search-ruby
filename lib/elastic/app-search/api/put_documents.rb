6b9127fec52074a37d42840798d8bddeba9d6b41
Fernando Briano <fernando@picandocodigo.net>
2021-09-28 15:08:26 +0100

    [App Search] Updates APIs to remove required query parameters exceptions
    Parameters previously required through the generated code dont throw an exception anymore and error
    is delegated to server. These should go through the body instead of part of the query parameters,
    but we still support sending them as query parameters in 7.x to keep backwards compatibility.
    
    APIs and parameters:
    - put_api_key
    - delete_meta_engine_source
    - put_curation
    - put_documents
    - schema

--- lib/elastic/app-search/api/put_documents.rb
+++ lib/elastic/app-search/api/put_documents.rb
@@ -22,17 +22,17 @@ module Elastic
     module AppSearch
       module Actions
         # Documents - Partial update of documents
+        # Update specific document fields by id and field
         #
-        # @param engine_name [String]  (*Required*)
+        # @param engine_name [String] Name of the engine (*Required*)
         # @param arguments [Hash] endpoint arguments
-        # @option arguments [Array] :documents List of documents to update (*Required*)
+        # @option arguments [Array] :documents
         # @option arguments [Hash] :headers optional HTTP headers to send with the request
         #
         # @see https://www.elastic.co/guide/en/app-search/current/documents.html#documents-partial
         #
         def put_documents(engine_name, arguments = {})
           raise ArgumentError, "Required parameter 'engine_name' missing" unless engine_name
-          raise ArgumentError, "Required parameter 'documents' missing" unless arguments[:documents]
 
           documents = arguments.delete(:documents) || {}
           headers = arguments.delete(:headers) || {}
