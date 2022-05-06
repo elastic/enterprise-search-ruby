c4dd7513c047c7fc0426b300c9b705f5ed73ef86
Fernando Briano <fernando@picandocodigo.net>
2022-05-06 18:04:35 +0100

    [App Search] Updates documents APIs

--- lib/elastic/app-search/api/index_documents.rb
+++ lib/elastic/app-search/api/index_documents.rb
@@ -24,20 +24,19 @@ module Elastic
         # Documents - Create a new document
         # Create or update documents
         #
-        # @param engine_name [String] Name of the engine (*Required*)
-        # @param arguments [Hash] endpoint arguments
-        # @option arguments [Array] :documents List of document to index (*Required*)
+        # @param [String] engine_name Name of the engine (*Required*)
+        # @param [Hash] arguments endpoint arguments
+        # @option arguments [Array<Hash> { id: [string] }] :documents
         # @option arguments [Hash] :headers optional HTTP headers to send with the request
         #
         # @see https://www.elastic.co/guide/en/app-search/current/documents.html#documents-create
         #
         def index_documents(engine_name, arguments = {})
           raise ArgumentError, "Required parameter 'engine_name' missing" unless engine_name
-          raise ArgumentError, "Required parameter 'documents' missing" unless arguments[:documents]
 
           documents = arguments.delete(:documents) || {}
-          headers = arguments.delete(:headers) || {}
 
+          headers = arguments.delete(:headers) || {}
           request(
             :post,
             "api/as/v1/engines/#{engine_name}/documents/",
