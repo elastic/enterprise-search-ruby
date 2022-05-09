c980e40b34e0fe08441a9da4ecf5012d77997466
Fernando Briano <fernando@picandocodigo.net>
2022-05-09 11:25:51 +0100

    [App Search] Updates search settings APIs

--- lib/elastic/app-search/api/reset_search_settings.rb
+++ lib/elastic/app-search/api/reset_search_settings.rb
@@ -24,9 +24,8 @@ module Elastic
         # Synonyms - Reset search settings
         # Resets search settings for the engine
         #
-        # @param engine_name [String] Name of the engine (*Required*)
-        # @param arguments [Hash] endpoint arguments
-        # @option arguments [Hash] :body The request body
+        # @param [String] engine_name Name of the engine (*Required*)
+        # @param [Hash] arguments endpoint arguments
         # @option arguments [Hash] :headers optional HTTP headers to send with the request
         #
         # @see https://www.elastic.co/guide/en/app-search/current/search-settings.html#search-settings-reset
@@ -34,14 +33,12 @@ module Elastic
         def reset_search_settings(engine_name, arguments = {})
           raise ArgumentError, "Required parameter 'engine_name' missing" unless engine_name
 
-          body = arguments.delete(:body) || {}
           headers = arguments.delete(:headers) || {}
-
           request(
             :post,
             "api/as/v1/engines/#{engine_name}/search_settings/reset/",
             arguments,
-            body,
+            nil,
             headers
           )
         end
