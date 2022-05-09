0d43209d7f01e8b730ef34b481b684cfa84a9ba1
Fernando Briano <fernando@picandocodigo.net>
2022-05-09 15:01:38 +0100

    [Enterprise Search] Updates API

--- lib/elastic/enterprise-search/api/read_only.rb
+++ lib/elastic/enterprise-search/api/read_only.rb
@@ -23,19 +23,18 @@ module Elastic
       # Read-Only - Get the read-only flag's state
       # Get the read-only flag's state
       #
-      # @param arguments [Hash] endpoint arguments
+      # @param [Hash] arguments endpoint arguments
       # @option arguments [Hash] :headers optional HTTP headers to send with the request
       #
       # @see https://www.elastic.co/guide/en/enterprise-search/current/read-only-api.html#getting-read-only-state
       #
       def read_only(arguments = {})
         headers = arguments.delete(:headers) || {}
-
         request(
           :get,
           'api/ent/v1/internal/read_only_mode/',
           arguments,
-          {},
+          nil,
           headers
         )
       end
