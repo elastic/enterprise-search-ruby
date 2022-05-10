1df8d97b73dcded10bee265ce321217735ff6427
Fernando Briano <fernando@picandocodigo.net>
2022-05-10 08:53:02 +0100

    [App Search] Adds Search Explain API

---
+++ lib/elastic/app-search/api/search_explain.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+module Elastic
+  module EnterpriseSearch
+    module AppSearch
+      module Actions
+        # SearchExplain - Retrieve the search query
+        # Submit a search and retrieve an Elasticsearch query
+        #
+        # @param [String] engine_name Name of the engine (*Required*)
+        # @param [Hash] arguments endpoint arguments
+        # @option arguments [Hash] :body (Required: query)
+        # @option body [string] :query
+        # @option body :analytics
+        # @option body :boost
+        # @option body :facets
+        # @option body :filters
+        # @option body :group
+        # @option body :page
+        # @option body :result_fields
+        # @option body :search_fields
+        # @option body :sort
+        # @option arguments [Hash] :headers optional HTTP headers to send with the request
+        #
+        # @see https://www.elastic.co/guide/en/app-search/current/search-explain.html
+        #
+        def search_explain(engine_name, arguments = {})
+          raise ArgumentError, "Required parameter 'engine_name' missing" unless engine_name
+          raise ArgumentError, "Required parameter 'body (query)' missing" unless arguments[:body]
+
+          body = arguments.delete(:body) || {}
+          headers = arguments.delete(:headers) || {}
+          request(
+            :post,
+            "api/as/v0/engines/#{engine_name}/search_explain/",
+            arguments,
+            body,
+            headers
+          )
+        end
+      end
+    end
+  end
+end
