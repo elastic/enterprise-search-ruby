ad0c203af58f4663bc9ffada4342a403292d9b58
Fernando Briano <fernando@picandocodigo.net>
2023-01-05 07:47:41 +0000

    [CI] Buildkite: Imports ci code to Buildkite

---
+++ .buildkite/log-results.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#
+# This script is intended to be run after the build in a Buildkite pipeline
+echo "--- Test summary"
+buildkite-agent artifact download "tmp/*.html" .
+
+files="tmp/*.html"
+for f in $files; do
+  SERVICE=`echo $f | grep -Po "(\d+\.)+\d+"`
+  RUBY_VERSION=`echo $f | grep -Po "(\d+\.)+\d+"`
+  EXAMPLES=`cat $f | grep -o "[0-9]\+ examples" | tail -1`
+  FAILURES=`cat $f | grep -o "[0-9]\+ failures" | tail -1`
+  PENDING=`cat $f | grep -o "[0-9]\+ pending" | tail -1`
+  echo "--- :rspec: $EXAMPLES - :x: $FAILURES - :pinched_fingers: $PENDING :test_tube: $SERVICE :ruby: $RUBY_VERSION"
+done
---
+++ .buildkite/run-tests.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+#
+# Script to run Enterprise Search container and Enterprise Search client integration tests on Buildkite
+#
+# Version 0.1
+#
+export TEST_SUITE=platinum
+export CONTAINER_NAME=enterprise-search
+
+script_path=$(dirname $(realpath -s $0))
+source $script_path/functions/imports.sh
+set -euo pipefail
+
+echo "--- Create the elastic network"
+docker network create elastic
+
+echo "--- :elasticsearch: Starting Elasticsearch"
+DETACH=true bash $script_path/run-elasticsearch.sh
+
+echo "--- :elastic-enterprise-search: Starting Enterprise Search"
+DETACH=true bash $script_path/run-enterprise-search.sh
+
+echo "+++ :ruby: Run Client"
+bash $script_path/run-client.sh
