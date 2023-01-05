4ba62b17cdf07b60c5e181e1032b1a724e8040ee
Fernando Briano <fernando@picandocodigo.net>
2023-01-05 10:50:24 +0000

    Addresses Rubocop offense

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -32,7 +32,10 @@ RSpec.configure do |config|
 
   config.add_formatter('documentation')
   config.add_formatter('RspecJunitFormatter', 'enterprise-search-junit.xml')
-  config.add_formatter('RSpec::Core::Formatters::HtmlFormatter', "tmp/enterprise-search-#{ENV['SERVICE']}-#{RUBY_VERSION}.html")
+  config.add_formatter(
+    'RSpec::Core::Formatters::HtmlFormatter',
+    "tmp/enterprise-search-#{ENV['SERVICE']}-#{RUBY_VERSION}.html"
+  )
 
   VCR.configure do |c|
     c.cassette_library_dir = 'spec/fixtures/vcr'
