9f6037c9f9f965b03d34c3c944b53aa888b76c2b
Fernando Briano <fernando@picandocodigo.net>
2023-01-05 11:16:14 +0000

    [CI] Buildkite: Updates failures/examples regexp in log results

--- .buildkite/log-results.sh
+++ .buildkite/log-results.sh
@@ -8,8 +8,8 @@ files="tmp/*.html"
 for f in $files; do
   SERVICE=`echo $f | grep -o "\(appsearch\|enterprisesearch\|workplacesearch\)"`
   RUBY_VERSION=`echo $f | grep -Po "(\d+\.)+\d+"`
-  EXAMPLES=`cat $f | grep -o "[0-9]\+ examples" | tail -1`
-  FAILURES=`cat $f | grep -o "[0-9]\+ failures" | tail -1`
+  EXAMPLES=`cat $f | grep -o "[0-9]\+ example" | tail -1`
+  FAILURES=`cat $f | grep -o "[0-9]\+ failure" | tail -1`
   PENDING=`cat $f | grep -o "[0-9]\+ pending" | tail -1`
   echo "--- :rspec: $EXAMPLES - :x: $FAILURES - :pinched_fingers: $PENDING :test_tube: $SERVICE :ruby: $RUBY_VERSION"
 done
