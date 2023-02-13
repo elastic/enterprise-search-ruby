1ea104de2d5256e65843ce022ce69e0533a556aa
Fernando Briano <fernando@picandocodigo.net>
2023-02-13 13:46:23 +0000

    [CI] Group services integration tests together in Buildkite

--- .buildkite/run-client.sh
+++ .buildkite/run-client.sh
@@ -8,28 +8,28 @@ set -euo pipefail
 
 export RUBY_VERSION=${RUBY_VERSION:-3.1}
 
-echo "--- Pinging Elasticsearch :elasticsearch:"
+echo "--- :telephone_receiver: Pinging Elasticsearch :elasticsearch:"
 curl --insecure --fail $external_elasticsearch_url/_cluster/health?pretty
 
 enterprise_search_url="http://localhost:3002"
-echo "--- Pinging Enterprise Search :elastic-enterprise-search:"
+echo "--- :telephone_receiver: Pinging Enterprise Search :elastic-enterprise-search:"
 curl -I --fail $enterprise_search_url
 
-echo "--- :ruby: Building Docker image"
+echo "--- :docker: :ruby: Building Docker image"
 docker build \
        --file $script_path/Dockerfile \
        --tag elastic/enterprise-search-ruby \
        --build-arg RUBY_VERSION=${RUBY_VERSION} \
        .
 
-echo "--- :ruby: Running $SERVICE tests"
+echo "--- :docker: :ruby: Running Client Container"
 docker run \
        --network ${network_name} \
        --name enterprise-search-ruby \
        --env "ELASTIC_ENTERPRISE_HOST=http://${CONTAINER_NAME}:3002" \
-       --env "SERVICE=${SERVICE}" \
+       --env "BUILDKITE=true" \
        --rm \
        --volume `pwd`:/code/enterprise-search-ruby \
        elastic/enterprise-search-ruby \
-       rake spec:integration:${SERVICE}
+       rake spec:integration
 
